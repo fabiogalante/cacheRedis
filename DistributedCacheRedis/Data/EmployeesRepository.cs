a667bab5eaf18f6f44b2bb9110b50747890423ab
Fabio Galante Mans <fabio.mans@grupoboticario.com.br>
2021-02-16 15:56:34 -0300

    json

--- DistributedCacheRedis/Data/EmployeesRepository.cs
+++ DistributedCacheRedis/Data/EmployeesRepository.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bogus;
 using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
+
+
 
 namespace DistributedCacheRedis.Data
 {
